9cc42f6c55330350f6752e653329dbc71aaf8fdf
druzdaniil <danyadruz2006@gmail.com>
2025-04-07 13:58:02 +0300

    signup.js

---
+++ signup.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const form = document.querySelector('form');
+	const nameInput = document.getElementById('name');
+	const gmailInput = document.getElementById('gmail');
+	const loginInput = document.getElementById('login');
+	const passwordInput = document.getElementById('password');
+ 
+	form.addEventListener('submit', function (e) {
+	  const name = nameInput.value.trim();
+	  const gmail = gmailInput.value.trim();
+	  const login = loginInput.value.trim();
+	  const password = passwordInput.value.trim();
+ 
+	  if (name === ''|| gmail === '' || login === '' || password === '') {
+		alert('Будь ласка, заповніть усі поля');
+	  } else {
+		location.assign('main_page.html')
+	  }
+	});
+ });
