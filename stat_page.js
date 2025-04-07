d4bdba42fefd66632e1f4137a811887e7fe53c66
druzdaniil <danyadruz2006@gmail.com>
2025-04-07 14:01:07 +0300

    stat_page files adding

---
+++ stat_page.js
@@ -0,0 +1,71 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+const main = document.querySelector('main');
+
+burger.addEventListener('click', () => {
+	burger.classList.toggle('active');
+	nav.classList.toggle('active');
+
+	if (burger.classList.contains('active')) {
+		main.style.zIndex = -10;
+	} else {
+		main.style.zIndex = 0;
+	};
+});
+
+const canvasWeek = document.getElementById('chart-week').getContext('2d');
+const canvasMonth = document.getElementById('chart-month').getContext('2d');
+
+let chartWeek = new Chart(canvasWeek, {
+	type: 'bar',
+	data: {
+		labels: ['Біг', 'Велосипед', 'Плавання', 'Йога', 'Ходьба', 'Тенніс'],
+		datasets: [{
+			label: 'Активність за тиждень (год)',
+			data: [1.43, 2.55, 5.11, 4.02, 5.65, 7.78],  //Приклад
+			backgroundColor: '#097e95',
+			borderColor: '#065471',
+			borderWidth: 3,
+			borderRadius: 5,
+		}]
+	},
+	options: {
+		maintainAspectRatio: false,
+		responsive: true,
+		layout: {
+			padding: {
+				top: 0,
+				left: 10,
+				right: 15,
+				bottom: 5,
+			}
+		}
+	}
+})
+
+let charMonth = new Chart(canvasMonth, {
+	type: 'bar',
+	data: {
+		labels: ['Біг', 'Велосипед', 'Плавання', 'Йога', 'Ходьба', 'Тенніс'],
+		datasets: [{
+			label: 'Активність за місяць (год)',
+			data: [4, 8, 16, 12, 8, 24],  //Приклад
+			backgroundColor: '#097e95',
+			borderColor: '#065471',
+			borderWidth: 3,
+			borderRadius: 5,
+		}]
+	},
+	options: {
+		maintainAspectRatio: false,
+		responsive: true,
+		layout: {
+			padding: {
+				top: 0,
+				left: 10,
+				right: 15,
+				bottom: 5,
+			}
+		}
+	}
+})
