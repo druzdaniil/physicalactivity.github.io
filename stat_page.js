e6d688a32495ada1d298b4562ecc47b840cdfe41
druzdaniil <danyadruz2006@gmail.com>
2025-04-14 19:46:16 +0300

    stat table animation

--- stat_page.js
+++ stat_page.js
@@ -53,4 +53,13 @@ let charMonth = new Chart(canvasMonth, {
 			}
 		}
 	}
-})
+});
+
+const tableContainer = document.querySelector('.table-container');
+document.addEventListener('mousemove', function(e) {
+	let dx = e.pageX - window.innerWidth / 2
+	let dy = e.pageY - window.innerHeight / 2
+	let angleX = 20 * dx / window.innerWidth / 2
+	let angleY = 20 * dy / window.innerHeight / 2
+	tableContainer.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+});
